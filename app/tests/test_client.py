c7982a1bf96cdab4afbdf04fb7bda0572023c157
Jald <Artem.Ryzhenkov@yandex.ru>
2023-10-11 21:47:35 +0300

    Fix test_config type var and req

--- app/tests/test_client.py
+++ app/tests/test_client.py
@@ -11,7 +11,7 @@ class TestConfig(TestCase):
         """Testing loading sensitive data from a file."""
         instance = Config(CONFIG_PATH=path.join(getcwd(), 'app', 'tests', 'test_data', 'config_test_data.ini'))
 
-        self.assertEqual('123456', instance.api_id)
+        self.assertEqual(123456, instance.api_id)
         self.assertEqual('abcdefg1234567', instance.api_hash)
         self.assertEqual('1234567:abcdefg1234567', instance.token1)
         self.assertEqual('7654321:7654321gfedcba', instance.token2)
